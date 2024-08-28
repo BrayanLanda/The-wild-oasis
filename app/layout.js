628e517ce659781b9613b999ddb28d8d8460c6a8
Brayan Acosta <landakkpp@gmail.com>
2024-08-28 16:53:24 -0500

    feat: add google font and icon

--- app/layout.js
+++ app/layout.js
@@ -2,18 +2,26 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css"
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export const metadata = {
   // title: "The Wild Oasis",
   title: {
     template: "%s The Wild Oasis",
     default: "Welcome / The Wild Oasis",
-  }
+  },
+  description: "Cabin hotel, located in the heart of the Colombia-Antioquia, surrounded by beatiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-primary-900 text-gray-50">
+      <body className={`${josefin.className} min-h-screen bg-primary-900 text-gray-50`}>
         <header>
           <Logo />
           <Navigation />
