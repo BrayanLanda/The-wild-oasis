c27b20d239dff6358b6fef75b14c68ddc51a3e7a
Brayan Acosta <landakkpp@gmail.com>
2024-08-28 18:35:19 -0500

    feat: add pages and images in cabinis, about and main layout

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "./_components/Navigation";
-import "@/app/_styles/globals.css"
+import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -15,19 +14,21 @@ export const metadata = {
     template: "%s The Wild Oasis",
     default: "Welcome / The Wild Oasis",
   },
-  description: "Cabin hotel, located in the heart of the Colombia-Antioquia, surrounded by beatiful mountains and dark forests",
+  description:
+    "Cabin hotel, located in the heart of the Colombia-Antioquia, surrounded by beatiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} min-h-screen bg-primary-900 text-gray-50`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <mian>{children}</mian>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased min-h-screen bg-primary-950 text-primary-100 flex flex-col`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
